8294029d60af6f21a9e4d78f2faa350606b1a40d
BEN OKELLO MWAKA <mwakaben@gmail.com>
2023-04-22 00:33:27 +0300

    resubale functions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,14 @@
 fun main() {
-    birthdayGreeting()
-}
-
-fun birthdayGreeting() : String {
-    val name ="Happy Birthday, Ben Okello!"
-    val age ="You are now 15 years old!"
-
-    return "$name /n $age";
-
+    println (match("22-April-2023", 134, "15",
+            "Rhinos", "Buffaloes", "Legends Rugby Club", "4:00 PM")); }
+fun match(fDate:String, fGameNo:Int, fGameWeek:String, fHomeTeam:String, fAwayTeam:String, fvenue:String, fTime:String,
+          ) : String {
+    val date ="$fDate"
+    val GameNo ="$fGameNo"
+    val GameWeek ="$fGameWeek"
+    val HomeTeam ="$fHomeTeam"
+    val AwayTeam ="$fAwayTeam"
+    val venue= "$fvenue"
+    val time = "$fTime"
+    return "$date \n NSSPL game no: $GameNo \n week $GameWeek \n $HomeTeam Vs $AwayTeam \n $venue \n $time";
 }
